e0e96f50121a3a87dcca379b2cc33c41cbf82765
Sachi Goyal <sachigoyal27@gmail.com>
2025-06-27 17:20:24 +0530

    .

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import PDFPageExtractor from "@/components/PdfExtrtact";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<div>
+  <PDFPageExtractor/>
+</div>
+  );
+}
