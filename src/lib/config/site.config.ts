f0c3d1ce5eae9e1e0e8d07cb8088db29a2394028
Sachi Goyal <sachigoyal27@gmail.com>
2025-06-27 18:34:34 +0530

    site config

---
+++ src/lib/config/site.config.ts
@@ -0,0 +1,32 @@
+import {SiteConfig} from "@/types/index"
+
+export const siteConfig: SiteConfig = {
+  name: "Page Picker",
+  title: "Page Picker - Your PDF Assistant",
+  description: "Page Picker is a tool that allows you to extract specific pages from your PDF documents.",
+  origin: "",
+  og: "",
+  keywords: ["pdf", "extraction", "page picker", "document", "tool", "pdf tool", "pdf extraction", "pdf page extraction", "pdf page picker"],
+  creator: {
+    name: "Sachii",
+    url: "https://sachi.dev",
+  },
+  socials: {
+    github: "https://github.com/sachigoyal/glimpsy",
+    x: "https://x.com/sachigoyal27",
+  },
+  contributors: [
+    {
+      name: "Sachi Goyal",
+      url: "https://sachi.dev",
+      github: "https://github.com/sachigoyal",
+      x: "https://x.com/sachigoyal27",
+    },
+    {
+      name: "Rudro",
+      url: "https://rdsx.dev",
+      github: "https://github.com/rudrodip",
+      x: "https://x.com/rds_agi",
+    }
+  ]
+};
